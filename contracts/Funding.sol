b38cfbc36540db2ae4d2ed8660678217dfe98596
Lucy Fang <lucyfang2@gmail.com>
2018-04-18 16:57:01 +0100

    map donation amounts to addresses

--- contracts/Funding.sol
+++ contracts/Funding.sol
@@ -5,11 +5,14 @@ contract Funding {
     address public owner;
     uint public raised;
 
+    mapping(address => uint) public balances;
+
     function Funding() public {
         owner = msg.sender;
     }
 
     function donate() public payable {
+        balances[msg.sender] += msg.value;
         raised += msg.value;
     }
 

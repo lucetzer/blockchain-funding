4681bf29c8b033825346c8b4170878987a9c9fb5
Lucy Fang <lucyfang2@gmail.com>
2018-04-18 16:14:37 +0100

    Add test about accepting donations from accounts

--- test/FundingTest.js
+++ test/FundingTest.js
@@ -2,6 +2,7 @@ var Funding = artifacts.require("./Funding.sol");
 
 contract("Funding", (accounts) => {
 
+  const FINNEY = 10 ** 15
   const [account1, account2] = accounts;
 
   it("shouldSetAnOwnerDuringCreation", async () => {
@@ -9,5 +10,13 @@ contract("Funding", (accounts) => {
     const owner = await contract.owner();
     assert.equal(owner, account1);
   });
+
+  it("accepts donations from accounts", async () => {
+    const contract = await Funding.deployed();
+    await contract.donate({from: account1, value: 10 * FINNEY});
+    await contract.donate({from: account2, value: 20 * FINNEY});
+    assert.equal(await contract.raised.call(), 30 * FINNEY);
+  });
+
 
 });

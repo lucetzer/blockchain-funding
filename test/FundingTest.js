98493833e7545df813e7bf8e4283bce6138692b9
Lucy Fang <lucyfang2@gmail.com>
2018-04-18 21:16:57 +0100

    added test for sending an event after a donation

--- test/FundingTest.js
+++ test/FundingTest.js
@@ -8,33 +8,35 @@ contract("Funding", (accounts) => {
   let contract;
 
   beforeEach('setup contract for each test', async () => {
-      contract = await Funding.new(DAY);
+      contract = await Funding.new();
+      await contract.donate({ from: account1, value: 10 * FINNEY });
+      await contract.donate({ from: account2, value: 20 * FINNEY });
   })
 
-  it("shouldSetAnOwnerDuringCreation", async () => {
+  it("shouldSetAnOwnerAndFundNameDuringCreation", async () => {
     const owner = await contract.owner();
     assert.equal(owner, account1);
   });
 
   it("accepts donations from accounts", async () => {
-    await contract.donate({ from: account1, value: 10 * FINNEY });
-    await contract.donate({ from: account2, value: 20 * FINNEY });
     assert.equal(await contract.raised.call(), 30 * FINNEY);
   });
 
   it("keeps track of donator balance", async () => {
-    await contract.donate({ from: account1, value: 5 * FINNEY });
-    await contract.donate({ from: account2, value: 15 * FINNEY });
     await contract.donate({ from: account2, value: 3 * FINNEY });
-    assert.equal(await contract.balances.call(account1), 5 * FINNEY);
-    assert.equal(await contract.balances.call(account2), 18 * FINNEY);
+    assert.equal(await contract.balances.call(account1), 10 * FINNEY);
+    assert.equal(await contract.balances.call(account2), 23 * FINNEY);
   });
 
-  it("finishes fundraising when time is up", async () => {
-    assert.equal(await contract.isFinished.call(), false);
-    await increaseTime(DAY);
-    assert.equal(await contract.isFinished.call(), true);
+  it("should create an event after a donation", async () => {
+    contract = await Funding.new();
+    return contract.donate({ from: account1, value: 10 * FINNEY }).then((response) => {
+      assert.equal(response.logs[0].event, "logDonation");
+      assert.equal(response.logs[0].args.sender, account1);
+      assert.equal(response.logs[0].args.amount, 10 * FINNEY);
   });
 
+
+  });
 
 });
